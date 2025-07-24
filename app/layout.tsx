f38a46478decaceaeb97b83de0ed966495f90d77
masfajaranugrah <fajaranugrah2203@gmail.com>
2025-07-24 20:50:01 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+             <head>
+                <meta name="google-site-verification" content="MYXdgUHJRuqNKWic4Chubn51VM7Q5PlnYN_5QBqKYc" />
+            </head>
             <body>{children}</body>
         </html>
     )
